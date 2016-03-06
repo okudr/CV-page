4d3f57c231d01a1ae0de11b03028c4a7e71ec7b6
okudr <sashko2@ua.fm>
2016-03-06 18:09:30 +0000

    Modify Style Script

--- js/jq-script.js
+++ js/jq-script.js
@@ -1,7 +1,13 @@
 $(function() {
   $('.flex-head-LR').hover(function() {
-    $('#flex-head-C').css('transform', 'rotateY(180deg)');
+    $('#flex-head-C').css('background', 'none');
         }, function() {
-    $('#flex-head-C').css('transform', '');
+    $('#flex-head-C').css('background', '');
   });
 });
+
+function showFunction() {
+    var x = document.getElementsByClassName("flex-body-BOX-LCR");
+    var i;
+    for (i = 0; i < x.length; i++) { x[i].style.display="flex"; }
+}
